36128a01431cefe8def0e9dde8738c37529ba71d
Yann Klein <yann.klein@me.com>
2022-05-02 21:56:03 +0900

    lecture base

---
+++ src/index.js
@@ -0,0 +1,49 @@
+console.log("Hi Batch #858!!!!");
+
+// ////////
+// Select DOM elements
+// ////////
+
+// with its ID (Shinji)
+
+// with its tag type (h2)
+
+// with its class (group)
+
+// by nest selection (ul of the group)
+
+// select multiple elements (all the card)
+
+
+// ////////
+// Modify the DOM
+// ////////
+
+// add an HTML element (Juventus de MediRechi)
+
+// same but more complex (add James!!!)
+
+// change the element style (group background to green)
+// Careful! background-color => backgroundColor
+
+// add a class to the element (add text-white to group)
+// Careful! no DOT for classList
+
+// remove it
+
+// add/remove every 1sec
+
+// Read/Write for inputs (change the value in the email input)
+
+// Read/Write the (inner) text and HTML content (FF Ronin 🥷 title)
+
+
+// ////////
+// Add event listeners
+// ////////
+
+// 1. Select elements (Shinji's picture)
+
+// 2. Listen to an event (a click on Shinji's picture)
+
+// 3. Change the DOM (add the .selected class top Shinji's picture)
